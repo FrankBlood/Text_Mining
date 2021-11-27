6d232d9ff69955dae5c9e8d5366785a784508717
frankblood <1062947208@qq.com>
2021-11-27 16:46:37 +0800

    model clear

--- Shallow/Base_Model.py
+++ Shallow/Base_Model.py
@@ -55,8 +55,7 @@ class Base_Model(object):
         if self.metrics_num == 4:
             metric = cal_all
         cal_res = metric(y, pred_y)
-        sorted_cal_res = sorted(cal_res, key=lambda x: x[0])
-        print(sorted_cal_res)
+        sorted_cal_res = sorted(cal_res.items(), key=lambda x: x[0])
         metric_names = '\t'.join([name_score[0][1:] for name_score in sorted_cal_res])
         metric_score = '\t'.join(['{:.2f}'.format(name_score[1]) for name_score in sorted_cal_res])
         if phase == 'train':
